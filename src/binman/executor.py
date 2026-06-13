d3c691118e478af6cee3a25d9b732db6c23964b3
dxvampi <dxvampi@gmail.com>
2026-06-14 01:39:04 +0200

    Added Windows and Mac compatibility

--- src/binman/executor.py
+++ src/binman/executor.py
@@ -1,19 +1,24 @@
-import os
+import subprocess
 import sys
 from typing import List, Optional
 from binman.models import Binary
 
+
 class Executor:
     def __init__(self, binary: Binary, args: Optional[List[str]] = None):
         self.binary = binary
-        # If no args are passed, empty list is returned
         self.args = args if args is not None else []
 
     def execute(self) -> None:
-        """Replaces actual process with binary."""
+        """Executes the binary"""
         if not self.binary.exists():
-            print(f"❌ Error: Binary for '{self.binary.alias}' does not exist in: {self.binary.path}")
+            print(f"Error: Binary for '{self.binary.alias}' does not exist in: {self.binary.path}")
             sys.exit(1)
 
-        # os.execv requires: (bin_path, [process_name, arg1, arg2, ...])
-        os.execv(self.binary.path, [self.binary.path] + self.args)
+        try:
+            result = subprocess.run([self.binary.path] + self.args)
+
+            sys.exit(result.returncode)
+
+        except KeyboardInterrupt:
+            sys.exit(130)
