1078eb631ae25967e75de4b77f26e718d4b87aba
dxvampi <dxvampi@proton.me>
2026-09-02 00:46:02 +0200

    docs: add config to help command

--- internal/cmd/help.go
+++ internal/cmd/help.go
@@ -8,7 +8,7 @@ func Help() {
 	fmt.Println()
 	fmt.Printf("-------- Binman help --------\n")
 	fmt.Println()
-	fmt.Println("'binman config' - Used to add new aliases")
+	fmt.Println("'binman config' || 'binman config <alias> <path>' - Used to add new aliases")
 	fmt.Println("'binman list' - Lists every alias that's saved on the config")
 	fmt.Println("'binman remove' - Used to remove aliases")
 	fmt.Println("'binman -b <alias> [args...]' - Runs a command with an alias")
