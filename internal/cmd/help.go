63fd823b6e038c9f2ec3d7b1f09e741a12126786
dxvampi <dxvampi@proton.me>
2026-09-02 00:52:19 +0200

    docs: add remove to help command

--- internal/cmd/help.go
+++ internal/cmd/help.go
@@ -10,7 +10,7 @@ func Help() {
 	fmt.Println()
 	fmt.Println("'binman config' || 'binman config <alias> <path>' - Used to add new aliases")
 	fmt.Println("'binman list' - Lists every alias that's saved on the config")
-	fmt.Println("'binman remove' - Used to remove aliases")
+	fmt.Println("'binman remove' || 'binman remove <alias>' - Used to remove aliases")
 	fmt.Println("'binman -b <alias> [args...]' - Runs a command with an alias")
 	fmt.Println("'binman which <alias>' - Prints the absolute path to the binary")
 	fmt.Println("'binman update/-U/--update' - Checks for updates and updates if possible")
